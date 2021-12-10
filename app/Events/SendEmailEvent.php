687af9fa32f642569d307bdb596f8b1588488a52
SabinaApostol <sabina.apostol@5psolutions.eu>
2021-12-10 18:12:25 +0200

    29 Test http://pm.5psolutions.net/public/index.php?path_info=projects/training-sabina-apostol/tasks/29 queue email event fix

--- app/Events/SendEmailEvent.php
+++ app/Events/SendEmailEvent.php
@@ -2,15 +2,10 @@
 
 namespace App\Events;
 
-use App\Mail\NewOrder;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Mail;
 
 class SendEmailEvent
 {
